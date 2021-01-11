00599c9156d225b2de560eada1364e8282f151a1
현석 송 <jhn06046@hotmail.com>
2021-01-11 21:54:52 +0900

    백준 1373

---
+++ 백준 풀어보기/2진수 8진수_1373/baekjoon_1373.py
@@ -0,0 +1,11 @@
+# 2진수가 주어졌을 때 8진수로 변환하는 문제
+pos = [1,2,4]
+
+if len(data)%3 == 1:
+    data = "00" + data
+elif len(data)%3 == 2:
+    data = "0" + data
+
+for i in range(0,len(data),3):
+    result = int(data[i+2])*1 + int(data[i+1])*2 + int(data[i])*4
+    print(result,end="")
